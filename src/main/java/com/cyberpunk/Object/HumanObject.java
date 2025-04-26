c4ce5af65336af1d7401211935ac4d22f1c418cc
VuzLe <tle63917@gmail.com>
2025-04-26 18:16:00 +0700

    Va chạm gốc và các phương thức của humanObject

--- src/main/java/com/cyberpunk/Object/HumanObject.java
+++ src/main/java/com/cyberpunk/Object/HumanObject.java
@@ -6,25 +6,36 @@ import com.cyberpunk.Effect.Animation;
 import com.cyberpunk.StartGame.GameFrame;
 
 public abstract class HumanObject extends Object{
+	public static final int HUMAN_HEIGHT = 34;
+	public static final int HUMAN_WIDTH = 21;
 	public static final float HUMAN_WEIGHT = 0.1f;
-	public static final float HUMAN_RUN_SPEED = 5;
-	public static final float HUMAN_WALK_SPEED = 2.5f;
+	public static final float HUMAN_RUN_SPEED = 3;
+	public static final float HUMAN_WALK_SPEED = 2f;
+	public static final long TIME_TO_CHANGE_DROP_STATE = 300000000L;
 	
-	private boolean isSitting;
-	private boolean isClimbing;
-	private boolean isDoubleJumping;
-	private boolean isSingleJumping;
+	private boolean isDrop = false;
+	private boolean isSitting = false;
+	private boolean isOnLadder, isClimbing;
+	private boolean isDoubleJumping = false;
+	private boolean isSingleJumping = false;
 	private boolean isLanding;
 	private boolean isOnGround;
 	private boolean isPhasing;
 	
 	private long noBeHurtDuration = 500000000L;
 	private long noBeHurtStart;
+	private long beginTime = 0;
 	
-	private Animation hurtForwardAni, hurtBackAni;
+	private Animation hurtForwardAnim, hurtBackAnim;
 	
 	public HumanObject(float x, float y, GameWorld gameWorld) {
-		super(x, y, 100, 10, 48, 48, gameWorld);
+		super(x, y, 100, 10, HUMAN_WIDTH, HUMAN_HEIGHT, gameWorld);
+		
+		isOnLadder = false;
+		isSitting = false;
+		isClimbing = false;
+		isDoubleJumping = false;
+		isSingleJumping = false;
 		
 		//set temp direction, team type
 		if(x > GameFrame.SCREEN_WIDTH / 2) {
@@ -35,6 +46,7 @@ public abstract class HumanObject extends Object{
 			 setTeamType(P1_TEAM);
 		}
 		
+		beginTime = System.nanoTime();
 		setMass(HUMAN_WEIGHT);
 	}
 
@@ -44,6 +56,17 @@ public abstract class HumanObject extends Object{
 
 	public void setSitting(boolean isSitting) {
 		this.isSitting = isSitting;
+	}
+	
+	public boolean getIsOnLadder() {
+		return isOnLadder;
+	}
+	
+	public void setOnLadder(boolean status) {
+		isOnLadder = status;
+		if(status) {
+			setSpeedY(0);
+		}
 	}
 
 	public boolean isClimbing() {
@@ -111,21 +134,29 @@ public abstract class HumanObject extends Object{
 	}
 
 	public Animation getHurtForwardAni() {
-		return hurtForwardAni;
+		return hurtForwardAnim;
 	}
 
 	public void setHurtForwardAni(Animation hurtForwardAni) {
-		this.hurtForwardAni = hurtForwardAni;
+		this.hurtForwardAnim = hurtForwardAni;
 	}
 
 	public Animation getHurtBackAni() {
-		return hurtBackAni;
+		return hurtBackAnim;
 	}
 
 	public void setHurtBackAni(Animation hurtBackAni) {
-		this.hurtBackAni = hurtBackAni;
+		this.hurtBackAnim = hurtBackAni;
 	}
 	
+	public boolean getIsDrop() {
+		return isDrop;
+	}
+
+	public void setIsDrop(boolean isDrop) {
+		this.isDrop = isDrop;
+	}
+
 	public abstract void jump();
 	public abstract void run();
 	public abstract void stopRun();
@@ -134,6 +165,37 @@ public abstract class HumanObject extends Object{
 	public abstract void attack();
 	public abstract void stopAttack();
 
+	public void startDrop(long time) {
+		if(!getIsDrop()) {
+			setSpeedY(getSpeedY() + 1);
+		}
+		
+	    this.isDrop = true;
+	    this.beginTime = time;
+//	    updateDropState();
+	}
+
+	public void updateDropState(long currentTime) {
+	    if (isDrop && currentTime - beginTime >= TIME_TO_CHANGE_DROP_STATE) {
+	        this.isDrop = false;
+	    }
+	}
+	
+	public void climbUp() {
+		isClimbing = true;
+		setSpeedY(-2);
+	}
+	
+	public void climbDown() {
+		isClimbing = true;
+		setSpeedY(2);
+	}
+	
+	public void stopClimb() {
+		isClimbing = false;
+		setSpeedY(0);
+	}
+	
 	public static final int x = 0;
 	public static final int y = 0;
 	public static final int z = 0;
@@ -168,56 +230,109 @@ public abstract class HumanObject extends Object{
 
 	@Override
 	public void Update() {
-		switch (getState()) {
-		case ALIVE:
-			//code for checking collision with object on map here
-			
-			//code for running
-//			run();
-			break;
+		updateDropState(System.nanoTime());
 		
-		case BEHURT:
-			setState(NOBEHURT);
-			isPhasing = true;
-			noBeHurtStart = System.nanoTime();
-			if(getHealth() <= 0) setState(DEATH);
-			break;
-			
-		case NOBEHURT:
-			//code for running
-			if(System.nanoTime() - noBeHurtStart > noBeHurtDuration) setState(ALIVE);
-//			run();
-			break;
-		
-		case DEATH:
-			//build death animation here
-			break;
-		default:
-			break;
+		if(getGameWorld().getMapGame().checkCollisionWithLadder(movingHitbox())) {
+			if(!isOnLadder) {
+				setOnLadder(true);
+			}
+		} else {
+			setOnLadder(false);
 		}
 		
-		if(getState() == ALIVE || getState() == NOBEHURT) {
-			//code for checking collision around character
-			setPosY(getPosY() + getSpeedY());
-			setSpeedY(getSpeedY() + getMass());
-			if(getPosY() >= 500) {
-				setSpeedY(0);
-				setPosY(500);
-				isOnGround = true;
-				isDoubleJumping = false;
-				isSingleJumping = false;
-				isLanding = false;
-			} else {
-				isOnGround = false;
-				setPosY(getPosY() + getSpeedY());
-				setSpeedY(getSpeedY() + getMass());
+			
+		setPosX(getPosX() + getSpeedX());
+		setPosY(getPosY() + getSpeedY());
+		Rectangle boundForCollisionWithMapFuture, hitBox;
+		
+		
+		boundForCollisionWithMapFuture = movingHitbox();
+//		boundForCollisionWithMapFuture.x += (getSpeedX()* Object.LEFT_DIR > 0 ? getSpeedX() * Object.LEFT_DIR : -1);
+		hitBox = getGameWorld().getMapGame().haveCollisionWithWallLeft(boundForCollisionWithMapFuture);
+		if (hitBox != null) {
+			if(getSpeedX() * Object.LEFT_DIR > 0) {
+				setPosX(hitBox.x + hitBox.width + getWidth() / 2 );
 			}
 		}
 		
-		if(getSpeedY() > 0) isLanding = true;
-//		System.out.println(getSpeedX() + " " + getSpeedY());
-//		System.out.println(isSingleJumping + " " + isDoubleJumping + " " + isOnGround);
-		setPosX(getPosX() + getSpeedX());
+		boundForCollisionWithMapFuture = movingHitbox();
+//		boundForCollisionWithMapFuture.x += (getSpeedX() != 0 ? getSpeedX() * Object.RIGHT_DIR : 1);
+		hitBox = getGameWorld().getMapGame().haveCollisionWithWallRight(boundForCollisionWithMapFuture);
+		if (hitBox != null) {
+			if(getSpeedX() * Object.RIGHT_DIR > 0) {
+				setPosX(hitBox.x - getWidth() / 2 - 1);
+			}
+		}
+		
+		boundForCollisionWithMapFuture = movingHitbox();
+		boundForCollisionWithMapFuture.y += (getSpeedY() != 0 ? getSpeedY() : 2);
+		hitBox = getGameWorld().getMapGame().haveCollisionWithLand(boundForCollisionWithMapFuture, this);
+		if (hitBox == null) {
+			if(!getIsOnLadder()) {
+				setSpeedY(getSpeedY() + getMass());
+			}
+		} else {
+			if(getSpeedY() > 0) {
+				setPosY(hitBox.y - getHeight() / 2);
+				setSpeedY(0);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+//		switch (getState()) {
+//		case ALIVE:
+//			//code for checking collision with object on map here
+//			
+//			break;
+//		
+//		case BEHURT:
+//			setState(NOBEHURT);
+//			isPhasing = true;
+//			noBeHurtStart = System.nanoTime();
+//			if(getHealth() <= 0) setState(DEATH);
+//			break;
+//			
+//		case NOBEHURT:
+//			//code for running
+//			if(System.nanoTime() - noBeHurtStart > noBeHurtDuration) setState(ALIVE);
+////			run();
+//			break;
+//		
+//		case DEATH:
+//			//build death animation here
+//			break;
+//		default:
+//			break;
+//		}
+//		
+//		if(getState() == ALIVE || getState() == NOBEHURT) {
+//			//code for checking collision around character
+//			setPosY(getPosY() + getSpeedY());
+//			setSpeedY(getSpeedY() + getMass());
+//			if(getPosY() >= 500) {
+//				setSpeedY(0);
+//				setPosY(500);
+//				isOnGround = true;
+//				isDoubleJumping = false;
+//				isSingleJumping = false;
+//				isLanding = false;
+//			} else {
+//				isOnGround = false;
+//				setPosY(getPosY() + getSpeedY());
+//				setSpeedY(getSpeedY() + getMass());
+//			}
+//		}
+//		
+//		if(getSpeedY() > 0) isLanding = true;
+////		System.out.println(getSpeedX() + " " + getSpeedY());
+////		System.out.println(isSingleJumping + " " + isDoubleJumping + " " + isOnGround);
+//		setPosX(getPosX() + getSpeedX());
 	}
 	
 }
