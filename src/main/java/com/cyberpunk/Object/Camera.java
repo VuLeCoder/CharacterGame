ca60625104fe13c554ffaa53cdc75b2290b4eb96
VuzLe <tle63917@gmail.com>
2025-05-07 14:57:55 +0700

    Thêm camera

---
+++ src/main/java/com/cyberpunk/Object/Camera.java
@@ -0,0 +1,103 @@
+package com.cyberpunk.Object;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import com.cyberpunk.StartGame.GamePanel;
+
+public class Camera {
+	private static final float OFFSET_X = 175;
+	private static final float OFFSET_Y = 150;
+	public static final float SCREEN_RATIO = (GamePanel.MAP_WIDTH) / (GamePanel.MAP_HEIGHT); // 2.0
+//	private static final float MIN_HEIGTH = 224;
+//	private static final float MIN_WIDTH = MIN_HEIGTH * SCREEN_RATIO;
+	
+	private float posX, posY;
+	private float widthView, heightView;
+	private BaseCharacter P1, P2;
+	
+	public Camera(float x, float y, float width, float height, GameWorld gameWorld) {
+		this.posX = x;
+		this.posY = y;
+		
+		this.widthView = width;
+		this.heightView = height;
+		
+		P1 = gameWorld.getP1();
+		P2 = gameWorld.getP2();
+	}
+	
+	public void setPosX(float x) {
+		this.posX = x;
+	}
+	
+	public void setPosY(float y) {
+		this.posY = y;
+	}
+	
+	public float getPosX() {
+		return posX;
+	}
+	
+	public float getPosY() {
+		return posY;
+	}
+	
+	public float getHeightView() {
+		return heightView;
+	}
+	
+	public float getWidthView() {
+		return widthView;
+	}
+	
+	public void Update() {
+		float minX = Math.min(P1.getPosX(), P2.getPosX());
+		float minY = Math.min(P1.getPosY(), P2.getPosY());
+		float maxX = Math.max(P1.getPosX(), P2.getPosX());
+		float maxY = Math.max(P1.getPosY(), P2.getPosY());
+		
+		float offsetLeftX = Math.min(minX, OFFSET_X);
+		float offsetRightX = Math.min(((GamePanel.MAP_WIDTH) - maxX), OFFSET_X);
+		
+		setPosX(minX - offsetLeftX);
+		widthView = maxX + offsetRightX - getPosX();
+		
+		float offsetUpY = Math.min(minY, OFFSET_Y);
+		float offsetDownY = Math.min(((GamePanel.MAP_HEIGHT) - maxY), OFFSET_Y);
+		
+		setPosY(minY - offsetUpY);
+		heightView = maxY + offsetDownY - getPosY();
+		
+		// code here
+		
+		float currentRatio = widthView / heightView;
+
+		if (currentRatio > SCREEN_RATIO) {
+		    // quá rộng → tăng heightView lên để đạt tỉ lệ
+		    float newHeight = widthView / SCREEN_RATIO;
+//		    float diff = newHeight - heightView;
+//		    setPosY(getPosY() - diff / 2); // camera cần dãn đều 2 bên
+		    heightView = newHeight;
+		} else if (currentRatio < SCREEN_RATIO) {
+		    // quá cao → tăng widthView lên để đạt tỉ lệ
+		    float newWidth = heightView * SCREEN_RATIO;
+//		    float diff = newWidth - widthView;
+//		    setPosX(getPosX() - diff / 2); // camera cần dãn đều 2 bên
+		    widthView = newWidth;
+		}
+		
+		if(getPosX() + widthView > GamePanel.MAP_WIDTH) {
+			setPosX(GamePanel.MAP_WIDTH - widthView);
+		}
+		
+		if(getPosY() + heightView > GamePanel.MAP_HEIGHT) {
+			setPosY(GamePanel.MAP_HEIGHT- heightView);
+		}
+	}
+	
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.cyan);
+		g2.drawRect((int) posX, (int)posY, (int)widthView, (int)heightView);
+	}
+}
--- src/main/java/com/cyberpunk/Object/GameWorld.java
+++ src/main/java/com/cyberpunk/Object/GameWorld.java
@@ -1,45 +1,50 @@
 package com.cyberpunk.Object;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import com.cyberpunk.Effect.DataLoader;
-import com.cyberpunk.StartGame.GameFrame;
+import com.cyberpunk.StartGame.GamePanel;
 
 public class GameWorld{
 	public static int TILESIZE = 32;
 	private static int pointX = 22, pointY = 14; // Điểm rơi hộp
 	
 	private final BufferedImage bufferedImage;
+	private final StartScreen startScreen;
 	private final MapGame mapGame;
 	private final int[][] animatedMap;
-	private final StartScreen startScreen;
 	
 	private boolean isStartGame = false;
 	private boolean isFirstDrawMap = true;
-//	private AnimatedObjectManager animatedObjectManager;
+
+	private Camera camera;
 	private ObjectManager objectManager;
 	
 	private BaseCharacter P1;
 	public BaseCharacter P2;
 
 	public GameWorld() {
-		bufferedImage = new BufferedImage(GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		bufferedImage = new BufferedImage(GamePanel.MAP_WIDTH, GamePanel.MAP_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		// bufferedImage = new BufferedImage(GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		
 		startScreen = new StartScreen();
 		mapGame = new MapGame();
 		animatedMap = DataLoader.getInstance().getAnimatedMap();
 
-		
-//		animatedObjectManager = new AnimatedObjectManager(this);
 		objectManager = new ObjectManager(this);
 		
 		P1 = new BaseCharacter(100, 500, "biker", this);
 		objectManager.addObject(P1);
 		
-		P2 = new BaseCharacter(600, 500, "biker", this);
+		
+		P2 = new BaseCharacter(100, 500, "biker", this);
 		objectManager.addObject(P2);
 		
+		camera = new Camera(0, 0, GamePanel.MAP_WIDTH, GamePanel.MAP_HEIGHT, this);
+//		camera = new Camera(GamePanel.MAP_DRAW_X, GamePanel.MAP_DRAW_Y, GamePanel.MAP_WIDTH, GamePanel.MAP_HEIGHT, this);
+
 		addAllAnimatedObject();
 	}
 	
@@ -93,6 +98,7 @@ public class GameWorld{
 			return;
 		}
 		
+		camera.Update();
 		objectManager.dropBox(pointX, pointY, System.nanoTime());
 		objectManager.UpdateObjects();
 //		while (!objectManager.checkCharacter()) {
@@ -100,7 +106,9 @@ public class GameWorld{
 //		}
 	}
 	
-	public void Render(float zoom) {
+	float currentZoomX = 1.0f, currentZoomY = 1.0f;
+	
+	public void Render() {
 		Graphics2D g2 = (Graphics2D) bufferedImage.getGraphics();
 		if(g2 == null) {
 			return;
@@ -110,14 +118,35 @@ public class GameWorld{
 			startScreen.draw(g2);
 			return;
 		}
+
+
 		
-//		g2.scale(2f, 2f);
+		g2.setColor(Color.BLACK);
+		// g2.fillRect(0, 0, GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT);
+		g2.fillRect(0, 0, GamePanel.MAP_WIDTH, GamePanel.MAP_HEIGHT);
+		
+		float targetZoomX = GamePanel.MAP_WIDTH / camera.getWidthView();
+		float targetZoomY = GamePanel.MAP_HEIGHT / camera.getHeightView();
+
+		// Hệ số làm mượt (càng gần 0 thì càng chậm, càng mượt)
+		float smoothFactor = 0.05f;
+
+		currentZoomX += (targetZoomX - currentZoomX) * smoothFactor;
+		currentZoomY += (targetZoomY - currentZoomY) * smoothFactor;
+		g2.scale(currentZoomX, currentZoomY);
+		
 //		if(isFirstDrawMap) {
 //			mapGame.draw(g2);
 //			isFirstDrawMap = false;
 //		}
-		g2.drawImage(mapGame.getCachedMapImage(), 0, 0, null);
 		
+		g2.translate(-camera.getPosX(),- camera.getPosY());
+		
+		// Vẽ map
+		g2.drawImage(mapGame.getCachedMapImage(), 0, 0, null);
+//		g2.drawImage(mapGame.getCachedMapImage(), GamePanel.MAP_DRAW_X, -GamePanel.MAP_DRAW_Y, null);
+		
+		camera.draw(g2);
 		objectManager.draw(g2);
 	}
 }
